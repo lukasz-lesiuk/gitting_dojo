1f5380341d3382d90975ead3409c7d4b07a388ec
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-03-22 11:00:28 +0100

    Added scream + merged versions

--- gittie.py
+++ gittie.py
@@ -1,14 +1,22 @@
 def introduce():
-    print("Hello, I'm Gittie! 0.1 version")
+    print("Hello, I'm Gittie!")
+    print("\n")
+
 
 def add(a, b):
     c = a + b
     return c
 
+
 def joke():
     print("Its a joke man :)")
 
-add(3, 7)
 
 def shout():
     print("AUUUUUU!")
+
+
+def shout2():
+    print("SCREEEAM")
+
+add(3, 7)
